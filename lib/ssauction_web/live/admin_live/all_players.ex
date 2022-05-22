3a7ca57568c601492cde94c15cc171ac04e3965e
Daryl Spitzer <dspitzer@roku.com>
2022-05-22 16:36:17 -0700

    add a form to the All Players page

--- lib/ssauction_web/live/admin_live/all_players.ex
+++ lib/ssauction_web/live/admin_live/all_players.ex
@@ -9,7 +9,8 @@ defmodule SSAuctionWeb.AdminLive.AllPlayers do
       socket
       |> assign(:year_and_league, "")
       |> assign(:num_players, Players.count_all_players())
-  
+      |> assign(:changeset, Ecto.Changeset.cast({%{}, %{}}, %{}, []))
+
     {:ok, socket, temporary_assigns: [all_players: []]}
   end
 
@@ -36,5 +37,17 @@ defmodule SSAuctionWeb.AdminLive.AllPlayers do
        |> assign(:num_players, num_players)
        |> assign(:all_players, all_players)
     }
+  end
+
+  @impl true
+  def handle_event("validate-change-param", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("change-param", params, socket) do
+    year_and_league = params["changeset"]["year_and_league"]
+
+    {:noreply, push_patch(socket, to: Routes.live_path(socket, SSAuctionWeb.AdminLive.AllPlayers, year_and_league: year_and_league))}
   end
 end
