c622dbb48f85f9dfff1768d1a22f9a7b76de14ae
Daryl Spitzer <dspitzer@alarm.com>
2026-03-14 12:14:44 -0700

    Fix race condition causing rostered players to disappear on refresh
    The PeriodicCheck handler for :roster_change broadcasts would query the
    database before transactions committed, caching stale data without the
    newly rostered player. Removed this handler since cache updates are
    already handled correctly by direct Cachex.put calls after DB operations
    complete in bids.ex and auctions.ex.

--- lib/ssauction/periodic_check.ex
+++ lib/ssauction/periodic_check.ex
@@ -2,7 +2,6 @@ defmodule SSAuction.PeriodicCheck do
   use GenServer
 
   alias SSAuction.Auctions
-  alias SSAuction.Auctions.Auction
 
   def start_link(default) when is_list(default) do
     GenServer.start_link(__MODULE__, default)
@@ -10,7 +9,6 @@ defmodule SSAuction.PeriodicCheck do
 
   @impl true
   def init(state) do
-    Auctions.subscribe() # Subscribe to auction broadcasts (e.g., roster_change)
     schedule_work() # Schedule work to be performed at some point
     {:ok, state}
   end
@@ -22,14 +20,6 @@ defmodule SSAuction.PeriodicCheck do
     Auctions.check_for_expired_nominations()
 
     schedule_work() # Reschedule once more
-    {:noreply, state}
-  end
-
-  @impl true
-  # also handle updating the auction_rostered_players cache here
-  def handle_info({:roster_change, auction = %Auction{}}, state) do
-    {:ok, true} = Cachex.put(:auction_rostered_players, auction.id,
-      Auctions.get_rostered_players_with_rostered_at_no_cache(auction))
     {:noreply, state}
   end
 
